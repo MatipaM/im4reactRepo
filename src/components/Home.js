3e233be81d6a2eb69be1ca7ce2ed8062d8d6e7ca
Matipa Marufu <2344503@students.wits.ac.za>
2023-06-06 09:05:42 +0200

    updated routing

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,14 @@ export default class Home extends Component {
     return (
       <div>
         <h1>
-        Internet Art
+        Internet Art Website
         </h1> 
         <article className="GameGrid">
-        {/* <section className="GameContainer">
-         <Spline scene="https://prod.spline.design/RaQFluFo5qvJH9JB/scene.splinecode" />
-         </section> */} 
+        <section className="GameContainer">
+        <Spline scene="https://prod.spline.design/i3GMYRMNbbNRyy9A/scene.splinecode" />
+        {/* <a href="https://my.spline.design/townvespaminigamecopy-62c67ccf93c44f7566ad2c67b7b99104/">see my spline scene</a> */}
+        
+         </section>
          <section className="pGrid">
          <p>Paragraph 1 about website</p>
          <p>Paragraph 2 about website</p>
