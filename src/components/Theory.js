36d3b22ae70a61bbfae6a74b23316508ad666236
Matipa Marufu <2344503@students.wits.ac.za>
2023-06-07 16:02:09 +0200

    update theory page

--- src/components/Theory.js
+++ src/components/Theory.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 import BlogCard from './BlogCard'
+import './Theory.css';
+import './General.css';
 
 export default class Theory extends Component {
   render() {
     return (
-      <div>
+      <article >
         <h1>
          Theory Page
         </h1>
-        <BlogCard title="Week 1 - " date = "12.01.01"/>
-     </div>
+        <section className="containerGrid">
+        <BlogCard heading="" subheading="Week 1" img="" altText = ""/>
+        <BlogCard heading="" subheading="Week 2" img="" altText = ""/>
+        <BlogCard heading="" subheading="Week 3" img="" altText = ""/>
+        <BlogCard heading="" subheading="Week 4" img="" altText = ""/>
+        <BlogCard heading="" subheading="Week 5" img="" altText = ""/>
+        <BlogCard heading="" subheading="Week 6" img="" altText = ""/>
+        <BlogCard heading="" subheading="Week 7" img="" altText = ""/>
+        <BlogCard heading="" subheading="Week 8" img="" altText = ""/>
+        <BlogCard heading="" subheading="Week 9" img="" altText = ""/>
+        <BlogCard heading="" subheading="Week 10" img="" altText = ""/>
+        <BlogCard heading="" subheading="Week 11" img="" altText = ""/>
+        <BlogCard heading="" subheading="Week 12" img="" altText = ""/>
+        <BlogCard heading="" subheading="Week 13" img="" altText = ""/>
+        </section>
+     </article>
     )
   }
 }
